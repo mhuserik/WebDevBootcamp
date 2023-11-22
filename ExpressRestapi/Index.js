83c43c7cc76425702bbefb2822c5fe4019c36854
mhuserik <46662827+mhuserik@users.noreply.github.com>
2023-11-22 11:11:43 -0800

    Made some progress

--- ExpressRestapi/Index.js
+++ ExpressRestapi/Index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
 
 app.get('/tacos', (req, res) => {
 res.send("GET /tacos response") 
